cedffc441c235924012718614201665acbd26c82
gmesz <gmesz@uwaterloo.ca>
2026-01-14 11:18:22 -0500

    Starting up the motor control file
    Added some variables.

--- WireStripper/Application/Src/Tasks/task_actuatorControl.c
+++ WireStripper/Application/Src/Tasks/task_actuatorControl.c
@@ -8,7 +8,34 @@
 //specific includes
 #include "task_actuatorControl.h"
 
+//PID Terms
+#define P1 = (float)1;
+#define I1 = (float)0;
+#define D1 = (float)0;
+#define P2 = (float)1;
+#define I2 = (float)0;
+#define D2 = (float)0;
+
+//Trajectories
+typedef enum {
+    IDLE,
+    STEP1,
+    STEP2,
+    STEP3,
+    STEP4,
+    STEP5
+}MotorStatus;
+MotorStatus motorStatus;
+
+//Motor Values
+int encoder1Step;
+int encoder2Step;
+
 void vActuatorTask(){
+    //Initialize variables
+    motorStatus = IDLE;
+    encoder1Step = 0;
+    encoder2Step = 0;
     for(;;){
         counterVar++;
         vTaskDelay(100);
